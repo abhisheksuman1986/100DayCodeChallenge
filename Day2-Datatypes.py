248d6833485d2d021c7a1fd9723c563074a1f146
Abhishek Suman <Abhishek.Suman@harman.com>
2024-07-16 09:07:07 +0530

    Adding Day1 and Day2 Files

---
+++ Day2-Datatypes.py
@@ -0,0 +1,36 @@
+# Date - 16 July 2024
+# Topics - Data Types, Numbers, Operations, Type Conversions, f-Strings
+# Note - 
+# project - 
+
+# Data Types
+
+# Strings 
+
+print("Hello"[0]) # Subscripting
+
+# Integer 
+
+print(123 + 456)
+
+# Large Integers - Some times we represent large no with comma like - 123,456,789
+# Even in python we can write large nos with underscore inplace of comma and python will treat them 
+# as numbers only
+
+print(123_456_789)
+
+# Float - With Decimals 
+
+3141.59
+
+# Boolean - True or False 
+
+print(True)
+print(False)
+
+print(type(734_529.678))
+
+
+
+
+
